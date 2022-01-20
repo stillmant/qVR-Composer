1c8c6de864689fafc6c6a23050176da6f3bed576
stillmant <stillmant98@gmail.com>
2022-01-19 21:58:18 -0800

    qSphere basic functionality

---
+++ qVR-Composer/Assets/Scripts/BasisState.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BasisState : MonoBehaviour
+{
+    [SerializeField] Renderer stickRenderer, sphereRenderer;
+    [SerializeField] GameObject probabilitySphere;
+
+    private Vector3 zeroState = Vector3.zero;
+    private Vector3 oneState = new Vector3(180, 0, 0);
+    
+    public void SetRotation(Vector3 newRotation)
+    {
+        var transformRotation = gameObject.transform.rotation;
+        transformRotation.eulerAngles = newRotation;
+    }
+    
+    public void ChangeColor(Color newColor)
+    {
+        stickRenderer.material.color = newColor;
+        sphereRenderer.material.color = newColor;
+    }
+
+    public void ScaleProbabilitySphere(Vector3 newScale)
+    {
+        probabilitySphere.transform.localScale = newScale;
+    }
+
+    public void SetToZeroState()
+    {
+        SetRotation(zeroState);
+    }
+
+    public void SetToOneState()
+    {
+        SetRotation(oneState);
+    }
+}
