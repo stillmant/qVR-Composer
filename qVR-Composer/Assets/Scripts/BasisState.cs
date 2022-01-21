23a07d43d0eb4919611996207281cdf3a04ca4f3
stillmant <stillmant98@gmail.com>
2022-01-20 22:25:35 -0800

    added basis labels and made them face the player at all times

--- qVR-Composer/Assets/Scripts/BasisState.cs
+++ qVR-Composer/Assets/Scripts/BasisState.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -7,9 +9,23 @@ public class BasisState : MonoBehaviour
 {
     [SerializeField] Renderer stickRenderer, sphereRenderer;
     [SerializeField] GameObject probabilitySphere;
+    [SerializeField] TextMeshPro basisLabel;
 
     private Vector3 zeroState = Vector3.zero;
     private Vector3 oneState = new Vector3(180, 0, 0);
+
+    private Transform playerTransform;
+    
+    private void Awake()
+    {
+        playerTransform = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        var playerTransformRotationrotation = playerTransform.rotation;
+        basisLabel.transform.LookAt(basisLabel.transform.position + playerTransformRotationrotation * Vector3.forward);
+    }
 
     public void SetRotation(Vector3 newRotation)
     {
@@ -30,10 +46,12 @@ public class BasisState : MonoBehaviour
     public void SetToZeroState()
     {
         SetRotation(zeroState);
+        basisLabel.text = "|0>";
     }
 
     public void SetToOneState()
     {
         SetRotation(oneState);
+        basisLabel.text = "|1>";
     }
 }
